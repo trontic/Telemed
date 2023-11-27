1f3bebaebf33a62de60188870ee7ff0427c5da9e
trontic <sinisa.androic@proton.me>
2023-11-27 04:10:56 +0100

    Poveznice

---
+++ resources/home-Jelena/static/script.js
@@ -0,0 +1,15 @@
+function redirectToLogin() {
+
+    window.location.href = '../../log-in-sinisa/static/log_in.html';
+}
+
+function redirectToPopisPacijenata() {
+
+    window.location.href = '../../popis-pacijenata-sinisa/static/popis_pacijenata.html';
+}
+
+function redirectToDodavanjePacijenta() {
+
+    window.location.href = '../../unos_novog_pacijenta-Fran/static/unos_novog_pacijenta.html';
+}
+
