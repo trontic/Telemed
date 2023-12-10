cc0984e7068acfa21b690b15cff2586148cf8a33
trontic <sinisa.androic@proton.me>
2023-12-10 12:42:54 +0100

    Promjena naziva html-a, dodavanje gumba, ponovno povezivanje html-a, promjene u script.js

--- resources/static/script.js
+++ resources/static/script.js
@@ -6,7 +6,7 @@ function login() {
     if (email === "doctor@gmail.com" && password === "doctor") {
         window.location.href = "doctor_home.html";
     } else if (email === "patient@gmail.com" && password === "patient") {
-        window.location.href = "patient_home.html";
+        window.location.href = "patient_home_lucija_lucic.html";
     } else {
         alert("Podaci za prijavu koje ste unijeli su pogrešni. Molim pokušajte ponovno.");
     }
@@ -16,4 +16,16 @@ function redirectToLogin() {
 
     var url = 'login.html'
     window.location.href = url;
+}
+
+function redirectNewPatient() {
+
+    var url = 'doctor_new_patient.html';
+        window.location.href = url;
+}
+
+function redirectNewData() {
+
+    var url = 'patient_new_data.html';
+        window.location.href = url;
 }
